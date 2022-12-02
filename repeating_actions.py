e489cd3f3307f7a331f275c59022e0a1e44f8a25
yan-maksi <102655652+yan-maksi@users.noreply.github.com>
2022-12-02 13:26:12 +0300

    Create repeating_actions.py

---
+++ repeating_actions.py
@@ -0,0 +1,24 @@
+""" 
+-- Task --
+Write a program that always asks the user to enter a number.
+When the user enters the negative number -1, the program should
+stop requesting the user to enter a number. The program must then
+calculate the average of the numbers entered excluding the -1. 
+"""
+
+user_input_list = []
+min_number = 0
+
+while True:
+    try:
+        num = int(input("Input an integer: "))
+    except ValueError:
+        num = int(input("Input an integer: "))
+
+    user_input_list.append(num)
+    if num <= min_number:
+        break
+
+user_input_list.pop()
+average = sum(user_input_list) / (len(user_input_list))
+print(f'Average of yours numbers {average}')
